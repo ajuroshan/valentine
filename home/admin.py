d39b5d1792ea97d7d3436cbf17af1b985d593e3e
ajuroshan <ajuaju0483@gmail.com>
2023-12-28 01:35:06 +0530

    added some search fields to the admin

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Applications,Interests,Match
 
-# Register your models here.
-admin.site.register(Applications)
+class ApplicationsAdmin(admin.ModelAdmin):
+    search_fields = ['user__username']  # Add other fields if needed
+
+
+class MatchAdmin(admin.ModelAdmin):
+    search_fields = ['male_application__user__username','female_application__user__username']
+admin.site.register(Applications, ApplicationsAdmin)
 admin.site.register(Interests)
-admin.site.register(Match)
+admin.site.register(Match,MatchAdmin)
